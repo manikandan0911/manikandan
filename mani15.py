bd611c8e7c07a908e4b906d1fcb0a34659ab8b6a
manikandan0911 <52271073+manikandan0911@users.noreply.github.com>
2019-07-15 22:42:36 -0700

    Update mani15.py

--- mani15.py
+++ mani15.py
@@ -1,4 +1,5 @@
-mm,mr=map(int,input().split())
-for i in range(mm+1,mr):
-    if(i%2==0):
-        print(i,end=" ")
+m,n = map(int,input().split())
+oddlist = []
+for i in range(m+1,n):
+	if i%2==0:
+		print(i,end=" ")
