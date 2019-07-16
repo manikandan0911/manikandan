04a57bd39e9759de3fb988146da83af48711803a
manikandan0911 <52271073+manikandan0911@users.noreply.github.com>
2019-07-15 21:21:12 -0700

    Create mani3.py

---
+++ mani3.py
@@ -0,0 +1,9 @@
+    
+mani=input("");
+if (mani>='a' and mani<='z') or (mani>='A' and mani<='Z'):
+	if (mani=='a' or mani=='e' or mani=='i' or mani=='o' or mani=='u' or mani=='A' or mani=='E' or mani=='I' or mani=='O' or mani=='U'):
+		print('Vowel')
+	else:
+		print('Consonant')
+else:
+	print('Invalid')
