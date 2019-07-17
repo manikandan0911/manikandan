ad8dd3c8093df7cbe7a32129f9d259af481d17a2
manikandan0911 <52271073+manikandan0911@users.noreply.github.com>
2019-07-16 21:33:09 -0700

    Create mani.52.py

---
+++ mani.52.py
@@ -0,0 +1,23 @@
+m1=int(input())
+if m1==1:
+    print("One")
+elif m1==2:
+    print("Two")
+elif m1==3:
+    print("Three")
+elif m1==4:
+    print("Four")
+elif m1==5:
+    print("Five")
+elif m1==6:
+    print("Six")
+elif m1==7:
+    print("seven")
+elif m1==8:
+    print("Eight")
+elif m1==9:
+    print("Nine")
+elif m1==10:
+    print("Ten")
+else:
+    print("no")
