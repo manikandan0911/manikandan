0591a7c325ef7ac9a101dc7e53de5278ec7464f9
manikandan0911 <52271073+manikandan0911@users.noreply.github.com>
2019-08-05 20:30:18 +0530

    Update mani.100.py

--- mani.100.py
+++ mani.100.py
@@ -1,6 +1,6 @@
-m=int(input("Enter number  :"))
+mani=int(input("Enter number  :"))
 count=0
-while(m>0):
+while(mani>0):
     count=count+1
-    m=m//10
+    mani=mani//10
 print("The number of digits in the number are:",count)
